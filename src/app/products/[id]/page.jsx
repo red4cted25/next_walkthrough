247fbf5a89a6f9cc2c053cda19602ffac190a8ef
Nico <141285419+red4cted25@users.noreply.github.com>
2025-02-18 13:04:40 -0700

    Update Slug
    Updated the slug to work properly

--- src/app/products/[id]/page.jsx
+++ src/app/products/[id]/page.jsx
@@ -7,18 +7,20 @@ export async function generateStaticParams() {
     }))
 }
 
-export default function ProductPage({ params }) {
-    const product = products.find((p) => p.id === params.id);
+export default async function ProductPage({ params }) {
+    // Fetch product data from your API or data source here
+    const paramId = await params;
+    const product = products.find((p) => p.id === paramId.id);
 
     if (!product) {
         return <h1>Product not found</h1>;
     }
 
     return (
-        <div className="container flex justify-center">
+        <div className="container flex flex-col item-center">
+            <Image src={product.image} alt={product.name} width={100} height={200} />
             <h1>{product.name}</h1>
             <p>{product.description}</p>
-            <Image src={product.image} alt={product.name} width={800} height={400} />
         </div>
     );
 }
